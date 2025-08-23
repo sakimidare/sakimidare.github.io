1cba642fd0459fa44f0f997822a858e9a7b48883
SakiMidare <ssmzhn@163.com>
2025-08-23 22:19:53 +0800

    Add Friends Support

--- src/config.ts
+++ src/config.ts
@@ -44,6 +44,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
+		LinkPreset.Friends,
 		{
 			name: "GitHub",
 			url: "https://github.com/sakimidare/sakimidare.github.io", // Internal links should not include the base path, as it is automatically added
